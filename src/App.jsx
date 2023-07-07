90700886bf246166cec91fc62308df1ec012fed2
s0d4z3r0 <av.falcao@hotmail.com>
2023-07-07 18:26:50 -0300

    Pagina inicial, ainda sem responsividade - tela 375

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Router, Routes, Link } from "react-router-dom";
+import Logo from './img/burger-logo-vector.png'
+import Burger from './img/burger.png'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div className="home">
+        <div className="logo">
+          <img src={Logo} alt="logo"/>
+        </div>
+        <div className="burger">
+          <img src={Burger} alt="burger" />
+        </div>
+        <div className="text">
+          <h3>Faça seu pedido agora mesmo!</h3>
+          <p>
+            A melhor hamburgueria, com os melhores ingredientes, possui os
+            melhores clientes.
+          </p>
+        </div>
+        <div className="btn">
+          <Link>FAZER PEDIDO</Link>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
