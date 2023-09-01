6834bd0b82889366527c0b2dc6b5c486e1c19b02
s0d4z3r0 <av.falcao@hotmail.com>
2023-09-01 18:11:07 -0300

    Responsividade da tela ErrorPage

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -1,28 +1,28 @@
 import { useNavigate } from "react-router-dom";
 import style from "./ErrorPage.module.css";
 import Logo from "../img/burger-logo-vector.png";
-import ErrorPng from '../assets/erro-404.png'
+import ErrorPng from "../assets/erro-404.png";
 import { useEffect, useState } from "react";
-import Footer from '../components/Footer/Footer'
+import Footer from "../components/Footer/Footer";
 
 const ErrorPage = () => {
   const [seconds, setSeconds] = useState(10);
 
   const navigate = useNavigate();
 
-// Conta regressivamente 10 segundos e redireciona a página para o Home
-  // useEffect(() => {
-  //    const redirect = setInterval(() => {
-  //       setSeconds(seconds-1)
+  // Conta regressivamente 10 segundos e redireciona a página para o Home
+  useEffect(() => {
+     const redirect = setInterval(() => {
+        setSeconds(seconds-1)
 
-  //       if(seconds === 1){
-  //           setSeconds(10)
-  //           return navigate('/totem-react')
-  //       }
-  //   }, 1000)
+        if(seconds === 1){
+            setSeconds(10)
+            return navigate('/totem-react')
+        }
+    }, 1000)
 
-  //   return () => clearInterval(redirect)
-  // })
+    return () => clearInterval(redirect)
+  })
 
   return (
     <div>
@@ -30,19 +30,24 @@ const ErrorPage = () => {
         <div className={style.logo}>
           <img src={Logo} alt="logo" />
         </div>
-        <div className={style.text}>
-          <h3>
-            Erro 404!
-          </h3>
+
+        <div className={style.imgErro}>
           <img src={ErrorPng} alt="error img" />
+        </div>
+
+        <div className={style.text}>
+          <h3>Erro 404!</h3>
           <p>
-            A página que você está procurando não
-            existe. <br />Redirecionando para a página
-            inicial em <span className={style.timer}>{seconds < 10 ? '0' + seconds : seconds}</span>...
+            A página que você está procurando não existe. <br />
+            Redirecionando para a página inicial em{" "}
+            <span className={style.timer}>
+              {seconds < 10 ? "0" + seconds : seconds}
+            </span>
+            ...
           </p>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
